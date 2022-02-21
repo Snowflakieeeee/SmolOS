002eeb83730b4655e0d0e682cea1e1a6b05f7ff8
Alumin112 <alumin112.welcome@gmail.com>
2022-02-21 15:38:56 +0530

    restructured code

--- os/src/kernel.rs
+++ os/src/kernel.rs
@@ -51,83 +51,7 @@ fn handle_kernel(files: &mut Vec<File>, character: char, type_mode: &mut bool, c
     }
     print!("{}", character);
     if character == '\n' {
-        match &*command.split_whitespace().collect::<Vec<_>>() {
-            [] => (),
-            ["clear"] => println!("\0"),
-            ["hi" | "hello"] => println!("hello :)"),
-            ["shut-down"] => println!(
-                "Shut down your computer using the power button, we haven't implemented that yet"
-            ),
-            ["os-info"] => {
-                println!("OS: SmolOS");
-                println!("Made in Rust");
-                println!("Made by: Bunch-of-cells, Catt & SnmLogic");
-            }
-            ["help"] => {
-                println!("Available commands:");
-                println!("     clear");
-                println!("     shut-down");
-                println!("     os-info");
-                println!("     help");
-                println!("     type");
-                println!("     ls");
-                println!("     save");
-                println!("     open");
-                println!("     delete");
-                println!("     save");
-            }
-            ["type"] => {
-                if let Some(File {content, ..}) = files.last() {
-                    *type_mode = true;
-                    println!(FG: Color::Black, BG: Color::LightGray, SCREEN: 1, "\0Press Esc to exit");
-                    print!(FG: Color::White, BG: Color::LightGray, SCREEN: 1, "{}", content)
-                } else {
-                    println!("No file opened");
-                }
-            }
-            ["new"] => {
-                if files.last().map(|x|x.name.is_none()).unwrap_or(false) {
-                    println!("Current file not saved");
-                } else {
-                    files.push(File::new());
-                }
-            }
-            ["save", filename] => {
-                if let Some(File { name, ..}) = files.last_mut() {
-                    *name = Some((*filename).to_owned());
-                } else {
-                    println!("No file has been opened");
-                }
-            }
-            ["open", filename] => {
-                if let Some(idx) = files.iter().position(|x| matches!(x.name, Some(ref s) if s == *filename)) {
-                    let len = files.len() - 1;
-                    files.swap(idx, len);
-                } else {
-                    println!("No such file found");
-                }
-            }
-            ["delete", filename] => {
-                if let Some(idx) = files.iter().position(|x| matches!(x.name, Some(ref s) if s == *filename)) {
-                    files.remove(idx);
-                } else {
-                    println!("No such file found");
-                }
-            }
-            ["what", "is" ,"cellulose?"] => {
-                println!("Cellulose is a type of organic compound that is found in the soil of plants. It is a natural building block for the synthesis of many other compounds. It is a polymer of Glucose");
-            }
-            ["poop"] => println!(FG: Color::Brown, "Someone just pooped ;-;"),
-            ["ls"] => {
-                println!("Available files:");
-                for file in files {
-                    if let Some(ref name) = file.name {
-                        println!("     {}", name);
-                    }
-                }
-            }
-            _ => println!(FG: Color::LightRed, "Unknown command: '{}'", command),
-        };
+        execute(command, type_mode, files);
         command.clear();
         print!(FG: Color::LightGreen, "demon@SmolOS:~/$ ");
         if *type_mode {
@@ -136,6 +60,86 @@ fn handle_kernel(files: &mut Vec<File>, character: char, type_mode: &mut bool, c
     } else if character != '\x08' {
         command.push(character);
     }
+}
+
+fn execute(command: &str, type_mode: &mut bool, files: &mut Vec<File>) {
+    match &*command.split_whitespace().collect::<Vec<_>>() {
+        [] => (),
+        ["clear"] => println!("\0"),
+        ["hi" | "hello"] => println!("hello :)"),
+        ["shut-down"] => println!(
+            "Shut down your computer using the power button, we haven't implemented that yet"
+        ),
+        ["os-info"] => {
+            println!("OS: SmolOS");
+            println!("Made in Rust");
+            println!("Made by: Bunch-of-cells, Catt & SnmLogic");
+        }
+        ["help"] => {
+            println!("Available commands:");
+            println!("     clear");
+            println!("     shut-down");
+            println!("     os-info");
+            println!("     help");
+            println!("     type");
+            println!("     ls");
+            println!("     save");
+            println!("     open");
+            println!("     delete");
+            println!("     save");
+        }
+        ["type"] => {
+            if let Some(File {content, ..}) = files.last() {
+                *type_mode = true;
+                println!(FG: Color::Black, BG: Color::LightGray, SCREEN: 1, "\0Press Esc to exit");
+                print!(FG: Color::White, BG: Color::LightGray, SCREEN: 1, "{}", content)
+            } else {
+                println!("No file opened");
+            }
+        }
+        ["new"] => {
+            if files.last().map(|x|x.name.is_none()).unwrap_or(false) {
+                println!("Current file not saved");
+            } else {
+                files.push(File::new());
+            }
+        }
+        ["save", filename] => {
+            if let Some(File { name, ..}) = files.last_mut() {
+                *name = Some((*filename).to_owned());
+            } else {
+                println!("No file has been opened");
+            }
+        }
+        ["open", filename] => {
+            if let Some(idx) = files.iter().position(|x| matches!(x.name, Some(ref s) if s == *filename)) {
+                let len = files.len() - 1;
+                files.swap(idx, len);
+            } else {
+                println!("No such file found");
+            }
+        }
+        ["delete", filename] => {
+            if let Some(idx) = files.iter().position(|x| matches!(x.name, Some(ref s) if s == *filename)) {
+                files.remove(idx);
+            } else {
+                println!("No such file found");
+            }
+        }
+        ["what", "is" ,"cellulose?"] => {
+            println!("Cellulose is a type of organic compound that is found in the soil of plants. It is a natural building block for the synthesis of many other compounds. It is a polymer of Glucose");
+        }
+        ["poop"] => println!(FG: Color::Brown, "Someone just pooped ;-;"),
+        ["ls"] => {
+            println!("Available files:");
+            for file in files {
+                if let Some(ref name) = file.name {
+                    println!("     {}", name);
+                }
+            }
+        }
+        _ => println!(FG: Color::LightRed, "Unknown command: '{}'", command),
+    };
 }
 
 struct File {
